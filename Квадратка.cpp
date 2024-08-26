17eb31a68d559c6cad12daace66c485d7effe1e2
SwampWood <118381294+SwampWood@users.noreply.github.com>
2024-08-26 14:15:31 +0300

    Уменьшение количества printf с switch
    Добавлен параметр roots -количество корней

--- Квадратка.cpp
+++ Квадратка.cpp
@@ -2,40 +2,54 @@
 #include <TXLib.h>
 #include <math.h>
 
+const int INF_ROOTS = -1;
+
 int main()
 {
     double a, b, c, d, x1 = 0, x2 = 0;
+    int roots = 0;
     scanf("%lg %lg %lg", &a, &b, &c); // Пользовательский ввод
     if (a == 0) // Проверка на нулевые коэффициенты
     {
         if (b == 0)
         {
             if (c == 0)
-                printf("x - любое число");
+                roots = INF_ROOTS;
             else
-                printf("Нет корней");
+                roots = 0;
         }
         else
         {
+            roots = 1;
             x1 = c / b;
-            printf("x = %lg", x1);
         }
     }
     else // Решение через дискриминант
     {
         d = b*b - 4*a*c;
         if (d < 0)
-            printf("Нет корней");
+            roots = 0;
         else if (d == 0)
         {
+            roots = 1;
             x1 = -b / (2 * a);
-            printf("x = %lg", x1);
         }
         else
         {
+            roots = 2;
             x1 = (-b - sqrt(d)) / (2 * a);
             x2 = (-b + sqrt(d)) / (2 * a);
-            printf("x1 = %lg, x2 = %lg", x1, x2);
         }
+    }
+    switch (roots)
+    {
+        case 0: printf("Нет корней");
+                break;
+        case 1: printf("x = %lg", x1);
+                break;
+        case 2: printf("x1 = %lg, x2 = %lg", x1, x2);
+                break;
+        case INF_ROOTS: printf("x - любое число");
+                break;
     }
 }
