a33127f87e6a8f646065f76959522a0a1016b59b
SwampWood <118381294+SwampWood@users.noreply.github.com>
2024-08-27 17:43:16 +0300

    Ещё больше разделение на функции, выход из программы с QUIT

--- Квадратка.cpp
+++ Квадратка.cpp
@@ -24,7 +24,11 @@ void InputCoeffs(struct Equation *eq);
 void SolveQuadratic(struct Equation *eq);
 void PrintSolution(struct Equation eq);
 int IsZero(double n);
-int ClearBuf(void); // очистка буфера ввода
+int ClearBuf(int *quit); // очистка буфера ввода
+void Linear (struct Equation *eq);
+void NegativeD (enum N_ROOTS *count_roots);
+void ZeroD (struct Equation *eq);
+void PositiveD (double d, struct Equation *eq);
 
 int main()
 {
@@ -43,14 +47,21 @@ void InputCoeffs(struct Equation *eq) // пользовательский ввод
 {
     int count_params = 0;
     int count_excess_ch = 0; // количество лишних символов (не пробелов) после ввода
+    int quit = 0;
+    printf("Введите коэффициенты квадратного уравнения\n"
+           "Чтобы выйти из программы, введите QUIT\n");
 
     while (true)
     {
         count_params = scanf("%lg %lg %lg", &(eq->a), &(eq->b), &(eq->c));
-        count_excess_ch = ClearBuf();
+        count_excess_ch = ClearBuf(&quit);
         if (count_params == 3 && count_excess_ch == 0)
         {
             break;
+        }
+        else if (count_params == 0 && quit)
+        {
+            exit(0);
         }
         else if (count_params != 3)
         {
@@ -70,40 +81,23 @@ void SolveQuadratic(struct Equation *eq)
 {
     if (IsZero(eq->a)) // проверка на нулевые коэффициенты
     {
-        if (IsZero(eq->b))
-        {
-            if (IsZero(eq->c))
-                eq->count_roots = INF_ROOTS;
-            else
-                eq->count_roots = NO_ROOTS;
-        }
-        else
-        {
-            eq->x1 = -eq->c / eq->b;
-
-            eq->count_roots = ONE_ROOT;
-        }
+        Linear(eq);
     }
     else
     {
         double d = (eq->b)*(eq->b) - 4*(eq->a)*(eq->c); // решение через дискриминант
+
         if (d < 0)
         {
-            eq->count_roots = NO_ROOTS;
+            NegativeD(&(eq->count_roots));
         }
         else if (IsZero(d))
         {
-            eq->x1 = -(eq->b) / (2 * (eq->a));
-
-            eq->count_roots = ONE_ROOT;
+            ZeroD (eq);
         }
         else
         {
-            double sqrt_d = sqrt(d);
-            eq->x1 = (-(eq->b) - sqrt_d) / (2 * (eq->a));
-            eq->x2 = (-(eq->b) + sqrt_d) / (2 * (eq->a));
-
-            eq->count_roots = TWO_ROOTS;
+            PositiveD (d, eq);
         }
     }
 }
@@ -125,15 +119,41 @@ void PrintSolution(struct Equation eq)
     }
 }
 
-int ClearBuf(void)
+int ClearBuf(int *quit)
 {
     int next_ch = '\0', count_not_spaces = 0;
+
+    int cur_quit = 0, failed_quit = 0; // текущий символ массива
+    char quit_string[4] = { 'Q', 'U', 'I', 'T' };
+
     while ((next_ch = getchar()) != '\n' && next_ch != EOF)
     {
         if (!isspace(next_ch))
         {
             ++count_not_spaces;
+
+            // проверяем, не вводит ли пользователь "QUIT"
+            if (cur_quit < 4 && next_ch == quit_string[cur_quit])
+            {
+                ++cur_quit;
+                if (cur_quit == 4)
+                {
+                    *quit = 1;
+                }
+            }
+            else
+            {
+                failed_quit = 1;
+            }
         }
+        else if (cur_quit)
+        {
+            failed_quit = 1;
+        }
+    }
+    if (failed_quit || count_not_spaces > 4)
+    {
+        *quit = 0;
     }
     return count_not_spaces;
 }
@@ -141,4 +161,43 @@ int ClearBuf(void)
 int IsZero(double n)
 {
     return (-EPSILON < n && n < EPSILON);
+}
+
+void Linear (struct Equation *eq)
+{
+    if (IsZero(eq->b))
+    {
+        if (IsZero(eq->c))
+            eq->count_roots = INF_ROOTS;
+        else
+            eq->count_roots = NO_ROOTS;
+    }
+    else
+    {
+        eq->x1 = -eq->c / eq->b;
+
+        eq->count_roots = ONE_ROOT;
+    }
+
+}
+
+void NegativeD (enum N_ROOTS *count_roots)
+{
+    *count_roots = NO_ROOTS;
+}
+
+void ZeroD (struct Equation *eq)
+{
+    eq->x1 = -(eq->b) / (2 * (eq->a));
+
+    eq->count_roots = ONE_ROOT;
+}
+
+void PositiveD (double d, struct Equation *eq)
+{
+    double sqrt_d = sqrt(d);
+    eq->x1 = (-(eq->b) - sqrt_d) / (2 * (eq->a));
+    eq->x2 = (-(eq->b) + sqrt_d) / (2 * (eq->a));
+
+    eq->count_roots = TWO_ROOTS;
 }
